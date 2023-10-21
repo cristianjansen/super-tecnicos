30483a6211120ba48488a865e985cba2592cee0b
cristianjansen <cristianjansen28@gmail.com>
2023-10-21 18:14:37 -0300

    super-tecnicos

---
+++ active_directory.ps1
@@ -0,0 +1,15 @@
+# Instalar a função AD-Domain-Services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "supertech.local" `
+-DomainNetbiosName "SUPERTECH" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
